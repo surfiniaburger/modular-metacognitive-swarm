c04efc95520fa8b6a1c1bfcd8319aa9120b90ef6
surfiniaburger <jdmasciano2@gmail.com>
2026-03-24 00:12:48 +0100

    Fix smoke test import

--- tools/a2a_smoke_test.py
+++ tools/a2a_smoke_test.py
@@ -1,6 +1,11 @@
 import asyncio
 import json
 import os
+import sys
+
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if ROOT_DIR not in sys.path:
+    sys.path.append(ROOT_DIR)
 
 from med_safety_gym.messenger import send_message
 
